57489c4feb5f8ec6c0a0e1bfb07a9615a0fd142f
imasharc <am0@tutamail.com>
2024-10-13 14:49:52 +0200

    test(NavigationTest): testNavigationToTimerPanel
    Run instrumented test to check if navigation to Timer panel is working
    
    1. check if there is image of Timer panel (on the home screen)
    2. click on that image (to navigate to Timer panel)
    3. check that screen title is 'Timer'

---
+++ app/src/androidTest/java/com/sharc/ramdhd/NavigationTest.kt
@@ -0,0 +1,31 @@
+package com.sharc.ramdhd
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NavigationTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testNavigationToTimerPanel() {
+        // Verify we're on the home screen
+//        onView(withId(R.id.navigation_home)).check(matches(isDisplayed()))
+        onView(withId(R.id.my_image_view)).check(matches(isDisplayed()))
+
+        // Click on the ImageView that navigates to the Timer panel
+        onView(withId(R.id.my_image_view)).perform(click())
+
+        // Check that the title of the screen is "Timer"
+        onView(withText("Timer")).check(matches(isDisplayed()))
+    }
+}
