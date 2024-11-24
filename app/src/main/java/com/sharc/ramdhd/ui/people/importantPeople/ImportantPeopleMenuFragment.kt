b810e2a8eee207ef01b66b790574744475ade507
imasharc <am0@tutamail.com>
2024-11-24 11:50:39 +0100

    feat/style(ImportantPeople menu FAB+EditSingleImportantEvent placeholder): add navigation and FAB to EditSingleImportantEvent view (with placeholder screen)

--- app/src/main/java/com/sharc/ramdhd/ui/people/importantPeople/ImportantPeopleMenuFragment.kt
+++ app/src/main/java/com/sharc/ramdhd/ui/people/importantPeople/ImportantPeopleMenuFragment.kt
@@ -3,29 +3,26 @@ package com.sharc.ramdhd.ui.people.importantPeople
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.sharc.ramdhd.R
+import com.sharc.ramdhd.databinding.FragmentImportantPeopleMenuBinding
 
-class ImportantPeopleMenuFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = ImportantPeopleMenuFragment()
-    }
-
+class ImportantPeopleMenuFragment : Fragment(R.layout.fragment_important_people_menu) {
+    private var _binding: FragmentImportantPeopleMenuBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: ImportantPeopleMenuViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentImportantPeopleMenuBinding.bind(view)
 
-        // TODO: Use the ViewModel
+        setupClickListeners()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_important_people_menu, container, false)
+    private fun setupClickListeners() {
+        binding.addNewEventFAB.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_important_people_menu_to_navigation_edit_single_important_event)
+        }
     }
 }
---
+++ app/src/main/java/com/sharc/ramdhd/ui/people/importantPeople/editSingle/EditSingleImportantEventViewModel.kt
@@ -0,0 +1,7 @@
+package com.sharc.ramdhd.ui.people.importantPeople.editSingle
+
+import androidx.lifecycle.ViewModel
+
+class EditSingleImportantEventViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
